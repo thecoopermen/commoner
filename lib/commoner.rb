5873ac288df802012c3372ca5934e4442aca6431
Jez Nicholson <jez.nicholson@gmail.com>
2015-01-05 07:52:16 +0000

    query categories
    list images which are categorised

--- lib/commoner.rb
+++ lib/commoner.rb
@@ -14,43 +14,79 @@ class Commoner
     new.images(term)
   end
 
+  def self.categorised_images(term)
+    new.categorised_images(term)
+  end
+
+  def self.details(title)
+    new.details(title)
+  end
+
   def initialize(base_uri = nil)
     @uri = (base_uri) ? base_uri : default_uri
   end
 
   def search(query)
+    puts search_uri(query)
     json_get(search_uri(query))[1]
   end
 
   def images(term)
     # get a list of titles for the given term
+    puts query_uri(term)
     response = json_get(query_uri(term))
     images   = response['query']['pages'].map { |page_id, page| page['images'] }
-    titles   = images.flatten.map { |image| image['title'] }
 
-    # map each title to category and url info
-    titles.map do |title|
-      response   = json_get(info_uri(title))
-      pages      = response['query']['pages'].map { |page_id, page| page }
-      categories = pages.first['categories'].map { |category| category['title'] }.flatten
-      descriptionurl = pages.first['imageinfo'].first['descriptionurl']
-
-      # description and author details are not available through the API calls
-      party = HTTParty.get(descriptionurl)
-      doc = Nokogiri::HTML(party.to_s)
-
-      author_url = ""
-      au = doc.xpath('//tr[td/@id="fileinfotpl_aut"]/td/a/@href')
-      author_url = au[0].content if au.size > 0
-      author_url = "http://commons.wikimedia.org" + author_url if author_url.start_with?('/wiki/User:')
-      {
-        categories:  categories.map { |category| category.gsub(/^Category:/, '') },
-        url:         pages.first['imageinfo'].first['url'],
-        description: Sanitize.clean(doc.xpath('//td[@class="description"]')[0].content)[0,255].strip!,
-        author:      Sanitize.clean(doc.xpath('//tr[td/@id="fileinfotpl_aut"]/td')[1].content),
-        author_url:  author_url
-      }
+    if images!=[nil]
+      titles   = images.flatten.map { |image| image['title'] }
+      # map each title to category and url info
+      titles.map do |title|
+        details(title)
+      end
     end
+  end
+
+  def categorised_images(category)
+    # get a list of titles for the given term
+    response = json_get(category_uri(category))
+    images = response['query']['categorymembers']
+
+    if images!=[nil]
+      titles = images.flatten.map { |image| image['title'] }
+      # map each title to category and url info
+      titles.map do |title|
+        puts title
+        if title.start_with?('Category:')
+          categorised_images(title)
+        else
+          details(title)
+        end 
+      end
+    end
+  end
+
+  def details(title)
+    return nil if (!title.start_with?('File:'))
+    response   = json_get(info_uri(title))
+    pages      = response['query']['pages'].map { |page_id, page| page }
+    categories = pages.first['categories'].map { |category| category['title'] }.flatten
+    descriptionurl = pages.first['imageinfo'].first['descriptionurl']
+
+    # description and author details are not available through the API calls
+    party = HTTParty.get(descriptionurl)
+    doc = Nokogiri::HTML(party.to_s)
+
+    author_url = ""
+    au = doc.xpath('//tr[td/@id="fileinfotpl_aut"]/td/a/@href')
+    author_url = au[0].content if au.size > 0
+    author_url = "http://commons.wikimedia.org" + author_url if author_url.start_with?('/wiki/User:')
+    {
+      categories:  categories.map { |category| category.gsub(/^Category:/, '') },
+      url:         pages.first['imageinfo'].first['url'],
+      description: Sanitize.clean(doc.xpath('//td[@class="description"]')[0].content)[0,255].strip!,
+      author:      Sanitize.clean(doc.xpath('//tr[td/@id="fileinfotpl_aut"]/td')[1].content),
+      author_url:  author_url
+    }
   end
 
 private
@@ -78,6 +114,10 @@ private
 
   def info_uri(image)
     uri_for action: 'query', titles: image, prop: 'imageinfo|categories', iiprop: 'url', format: 'json'
+  end
+
+  def category_uri(term)
+    uri_for action: 'query', cmtitle: term, list: 'categorymembers', format: 'json'
   end
 
   def default_uri
--- spec/commoner_spec.rb
+++ spec/commoner_spec.rb
@@ -1,26 +1,83 @@
 require 'spec_helper'
 
 describe Commoner do
-  it 'searches for stuff' do
-  	titles = Commoner.search("Meles meles")
-#    titles.each_with_index do |t, index|
-#      puts index.to_s + ". " + t
-#    end
-    expect(titles[1].to_s).to eq("Meles anakuma")
+
+  describe '#search' do
+    context 'on a term that is not listed' do
+      it 'returns an empty array' do
+        titles = Commoner.search("badger")
+        expect(titles).to eq([])
+      end
+    end
+
+    context 'on a known term' do
+      it 'returns some titles' do
+        titles = Commoner.search("Meles meles")
+        expect(titles.size).to be > 0
+      end
+    end
+
+    context 'humph' do
+      it 'searches for stuff' do
+        titles = Commoner.search("plaques")
+        titles.each_with_index do |t, index|
+#          puts index.to_s + ". " + t
+        end
+      end
+    end
   end
 
-  it 'list images for a page' do
-  	images = Commoner.images("Meles meles")
-#  	images.each_with_index do |image, index|
-#  	  puts index.to_s + ". " 
-#      puts "  url: " + image[:url]
-#      puts "  description: " + image[:description]
-#      puts "  author: " + image[:author] 
-#      puts "  author_url: " + image[:author_url]
-#      puts " "
-#  	end
-    first = images[0]
-    expect(first[:url].start_with?("http:")).to be(true)
+  describe '#images' do
+
+    it 'list images for a page' do
+    	images = Commoner.images("Meles meles")
+  #  	images.each_with_index do |image, index|
+  #  	  puts index.to_s + ". " 
+  #      puts "  url: " + image[:url]
+  #      puts "  description: " + image[:description]
+  #      puts "  author: " + image[:author] 
+  #      puts "  author_url: " + image[:author_url]
+  #      puts " "
+  #  	end
+      first = images[0]
+      expect(first[:url].start_with?("http:")).to be(true)
+    end
+
+    it 'copes when there are no images found' do
+      images = Commoner.images("plaques")
+    end
+
   end
 
+  describe '#details' do
+
+    context 'a non-file page name' do
+      it 'gives details of a title' do
+        images = Commoner.details("Meles meles")
+      end
+    end
+
+    context 'an unknown page name' do
+      it 'gives details of a title' do
+        images = Commoner.details("File:xyz.jpg")
+      end
+    end
+
+    context 'a Wikimedia File page' do
+      it 'gives details of a title' do
+        images = Commoner.details("File:Badger 25-07-09.jpg")
+      end
+    end
+  end
+
+  describe '#details' do
+
+    context 'a base category' do
+      it 'lists images categorised as this and within subcategories' do
+        images = Commoner.categorised_images("Category:Stolpersteine")
+        puts images.to_s
+      end
+    end
+
+  end
 end
